3920cce645d1a20a207812f9a930c3ef346cff7d
Gabriel Pietroluongo <gabrielpietroluongo@gmail.com>
2022-09-24 00:57:14 -0300

    Added missing properties to clothing
    Also removed tests that didn't make sense anymore

--- src/main/kotlin/store/Clothing.kt
+++ src/main/kotlin/store/Clothing.kt
@@ -1,6 +1,9 @@
 package com.github.pietroluongo.store
+enum class ClothingSizes {
+    PP, P, M, G, GG, XG, XXG
+}
 
-class Clothing constructor(override val name: String, override val purchasePrice: Double, override var sellingPrice: Double, productCode: String) : Product {
+class Clothing constructor(override val name: String, override val purchasePrice: Double, override var sellingPrice: Double, productCode: String, val size: ClothingSizes, val primaryColor: String, val secondaryColor: String? = null) : Product {
     override val productCode = productCode
         get() {
             return "C-${field.uppercase()}"
--- src/test/kotlin/store/ClothingTest.kt
+++ src/test/kotlin/store/ClothingTest.kt
@@ -1,12 +1,13 @@
 package store
 import org.junit.jupiter.api.*
 import com.github.pietroluongo.store.Clothing
+import com.github.pietroluongo.store.ClothingSizes
 
 const val CLOTHING_IDENTIFIER = "cloth"
 
 class ClothingTest {
-    val clothing = Clothing("Clothing name", 0.0, 0.0, CLOTHING_IDENTIFIER, ClothingType.M, "red", "blue");
-    val clothingWithoutSecondaryColor = Clothing("Clothing name", 0.0, 0.0, "$CLOTHING_IDENTIFIER-NOCOLOR", ClothingType.M, "red");
+    val clothing = Clothing("Clothing name", 0.0, 0.0, CLOTHING_IDENTIFIER, ClothingSizes.M, "red", "blue");
+    val clothingWithoutSecondaryColor = Clothing("Clothing name", 0.0, 0.0, "$CLOTHING_IDENTIFIER-NOCOLOR", ClothingSizes.M, "red");
     @Test
     @DisplayName("Clothes creation")
     fun testCreateClothes() {
@@ -16,7 +17,7 @@ class ClothingTest {
     @Test
     @DisplayName("Clothes identifier")
     fun testClothingIdentifier() {
-        Assertions.assertEquals("C-cloth", clothing.name)
+        Assertions.assertEquals("C-CLOTH", clothing.productCode)
     }
 
 }
